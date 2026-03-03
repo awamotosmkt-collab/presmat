fd0da0672d5002d07b3746fb6ef53a04b0da22a6
awamotosmkt-collab <awamotos.mkt@gmail.com>
2026-03-02 22:00:21 -0300

    feat: add root files and config

---
+++ config/config.example.php
@@ -0,0 +1,38 @@
+<?php
+define('PMS_SITE_TITLE', 'My Site');
+define('PMS_TIME_ZONE', 'America/Sao_Paulo');
+define('PMS_DATE_FORMAT', '%d/%m/%Y');
+define('PMS_TIME_FORMAT', '%I:%M%P');
+define('PMS_CURRENCY_ENABLED', '0');
+define('PMS_CURRENCY_POS', 'before'); // "before" or "after"
+define('PMS_LANG_ENABLED', '1');
+define('PMS_ADMIN_LANG_FILE', 'pt_BR.ini');
+define('PMS_ENABLE_COOKIES_NOTICE', '1');
+define('PMS_MAINTENANCE_MODE', '0');
+define('PMS_MAINTENANCE_MSG', '<h1>Em breve...</h1><p>Site em manutenção.</p>');
+define('PMS_TEMPLATE', 'default');
+define('PMS_OWNER', 'My Site');
+define('PMS_EMAIL', 'contact@example.com');
+define('PMS_ADDRESS', '');
+define('PMS_PHONE', '');
+define('PMS_MOBILE', '');
+define('PMS_FAX', '');
+define('PMS_DB_NAME', 'pandao_cms');
+define('PMS_DB_HOST', '127.0.0.1');
+define('PMS_DB_PORT', '3306');
+define('PMS_DB_USER', 'root');
+define('PMS_DB_PASS', 'YOUR_PASSWORD_HERE');
+define('PMS_SENDER_EMAIL', 'admin@example.com');
+define('PMS_SENDER_NAME', 'My Site');
+define('PMS_USE_SMTP', '0');
+define('PMS_SMTP_SECURITY', '');
+define('PMS_SMTP_AUTH', '0');
+define('PMS_SMTP_HOST', '');
+define('PMS_SMTP_USER', '');
+define('PMS_SMTP_PASS', '');
+define('PMS_SMTP_PORT', '25');
+define('PMS_GMAPS_API_KEY', '');
+define('PMS_ANALYTICS_CODE', '');
+define('PMS_ADMIN_FOLDER', 'admin');
+define('PMS_CAPTCHA_PKEY', ''); // ReCAPTCHA public key
+define('PMS_CAPTCHA_SKEY', ''); // ReCAPTCHA secret key
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+// Main entry of the front office is public/index.php
+
+echo 'Wrong way \u00af\_(\u30c4)_/\u00af';
+exit;
